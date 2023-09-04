c7ea62df16783d483e7415dbf9dfb2d2b055fc0b
Rashevmarina <Rashevmarina@gmail.com>
2023-09-04 09:40:27 -0700

    ch

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const ollCategoriesEl = categories => {
 	const elementItems = document.querySelectorAll(categories);
-	console.log(`Number of categories: ${itemsEl.length}`);
+	console.log(`Number of categories: ${elementItems.length}`);
     elementItems.forEach(item => {
         console.log(`Category: ${item.firstElementChild.textContent}`),
         console.log(`Number of elements: ${item.lastElementChild.children.length}`)
